0fccd5108ad126467a3f16f3b3a6f4c492cd1628
martin0199 <1084845866@qq.com>
2021-01-05 18:39:39 +0800

    dev

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import '@/assets/reset.css'
+import '@/assets/fonts/iconfont.css'
 createApp(App).use(store).use(router).mount('#app')
